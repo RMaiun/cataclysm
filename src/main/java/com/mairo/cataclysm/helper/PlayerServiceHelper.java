0584eed8d3de90ecd322bbb567b9ed123ececf61
Roman Maiun <roman.maiun@unicorn.com>
2020-11-30 21:34:21 +0200

    player service log

--- src/main/java/com/mairo/cataclysm/helper/PlayerServiceHelper.java
+++ src/main/java/com/mairo/cataclysm/helper/PlayerServiceHelper.java
@@ -5,10 +5,14 @@ import com.mairo.cataclysm.exception.PlayersNotFoundException;
 import io.vavr.control.Either;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PlayerServiceHelper {
+
+  public static final Logger log = LogManager.getLogger(PlayerServiceHelper.class);
 
   public List<String> lowercaseSurnames(List<String> surnameList) {
     return surnameList.stream()
@@ -17,6 +21,11 @@ public class PlayerServiceHelper {
   }
 
   public Either<Throwable, List<Player>> prepareCheckedPlayers(List<Player> players, List<String> surnames) {
+    String found = players.stream()
+        .map(Player::getSurname)
+        .collect(Collectors.joining(","));
+    String expected = String.join(",", surnames);
+    log.info("Expected players: {}, found players: {}", expected, found);
     if (players.size() == surnames.size()) {
       return Either.right(players);
     } else {
---
+++ src/main/java/com/mairo/cataclysm/processor/AddPlayerCmdProcessor.java
@@ -0,0 +1,10 @@
+package com.mairo.cataclysm.processor;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AddPlayerCmdProcessor {
+
+}
